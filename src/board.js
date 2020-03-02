b0df41233474c3a67bde38188fb67266276a2f3d
Rafael Herrera <santoElias65537@gmail.com>
2020-03-01 22:31:29 -0700

    got it so if you click on bomb on first click, removes bomb and all surrounding bombs aswell

--- src/board.js
+++ src/board.js
@@ -21,7 +21,7 @@ export default class Board extends Component {
         this.createNestedArray(3, 9);
     };
 
-    randomTrueFalse = () => Math.random() <= 0.2;
+    randomTrueFalse = () => Math.random() <= 0.9;
 
     createNestedArray = (x, y) => {
         //x is how many arrays
@@ -57,7 +57,8 @@ export default class Board extends Component {
         }
 
         for (let i = 0; i < idsOfBombsHere.length; i++) {
-            this.incrementNearbyCellBomb(nestedArray, idsOfBombsHere[i])
+            //increments all nearby cells if its a bomb
+            this.allNearbyCells(nestedArray, idsOfBombsHere[i], (bomb) => { bomb.nearbyBombs++ })
         }
 
         this.setState({
@@ -66,42 +67,6 @@ export default class Board extends Component {
             bombsOnBoard: countBombs,
             idsOfBombs: idsOfBombsHere
         })
-    }
-    //need to abstracct these
-    incrementNearbyCellBomb(board, stringId) {
-        let Id = this.stringToId(stringId);
-
-        const x = Id[0];
-        const y = Id[1];
-
-        for (let i = x - 1; i <= x + 1; i++) {
-            for (let j = y - 1; j <= y + 1; j++) {
-                //if it dont exist, move on
-                if (!board[i] || !board[i][j]) { continue; }
-                //dont put the box you clicked on the array of ids that represent the surrounding ids
-                else if (i === x && j === y) { continue; }
-
-                board[i][j].nearbyBombs++;
-            }
-        }
-    }
-
-    decrementNearbyCellBomb(board, stringId) {
-        let Id = this.stringToId(stringId);
-
-        const x = Id[0];
-        const y = Id[1];
-
-        for (let i = x - 1; i <= x + 1; i++) {
-            for (let j = y - 1; j <= y + 1; j++) {
-                //if it dont exist, move on
-                if (!board[i] || !board[i][j]) { continue; }
-                //dont put the box you clicked on the array of ids that represent the surrounding ids
-                else if (i === x && j === y) { continue; }
-
-                board[i][j].nearbyBombs--;
-            }
-        }
     }
 
     //for every item in array creates cell
@@ -115,6 +80,7 @@ export default class Board extends Component {
                 {/*if is hasnt been clicked display a question mark, else deplending on the situation display some kind of logic */}
                 {this.state.board[x][y].screen}
                 {`${this.state.board[x][y].isBomb}`}
+                {this.state.board[x][y].nearbyBombs}
             </td>);
             return (
                 <tr>
@@ -137,6 +103,8 @@ export default class Board extends Component {
         let ID = this.stringToId(e.target.id)
         let cellClicked = this.state.board[ID[0]][ID[1]];
 
+        //console.log(this.state.idsOfBombs)
+
         if (cellClicked.hasBeenClicked) { return; }
         //if you die on first click, fix that
         if (cellClicked.isBomb && this.state.firstClick) {
@@ -151,12 +119,14 @@ export default class Board extends Component {
 
 
         //disgusting!!!!, refactor
-       this.changeCellScreen(newBoard, e.target.id);
+        this.changeCellScreen(newBoard, e.target.id);
 
         this.setState({
             board: newBoard,
             firstClick: false
         });
+
+        //console.log(this.state.idsOfBombs)
     }
 
     changeCellScreen(currentBoard, id) {
@@ -170,8 +140,8 @@ export default class Board extends Component {
         else { currentBoard[ID[0]][ID[1]].screen = `${currentBoard[ID[0]][ID[1]].nearbyBombs}` }
 
     }
-
-    showNearbyCellBombScreen(board, stringId) {
+    //gets all nearby cells
+    allNearbyCells(board, stringId, fn) {
         let Id = this.stringToId(stringId);
 
         const x = Id[0];
@@ -182,52 +152,60 @@ export default class Board extends Component {
                 //if it dont exist, move on
                 if (!board[i] || !board[i][j]) { continue; }
                 //dont put the box you clicked on the array of ids that represent the surrounding ids
-                //else if (i === x && j === y) { continue; }
+                if (i === x && j === y) { continue; }
 
-                this.changeCellScreen(board, `${i}-${j}`)
+                fn(board[i][j]);
             }
         }
     }
 
+    getCell(board, cellID, fn) {
+        //modify one cell
+        let Id = this.stringToId(cellID)
+
+        fn(board[Id[0]][Id[1]]);
+    }
 
     removeItem(arr, removeThis) {
-        return arr.filter(item => item !== removeThis)
+        let newArr = arr.filter(item => item !== removeThis);
+        return newArr;
+    }
+
+    removeBomb(newBoard, cellID) {
+        //makes the cell bomb false
+        this.getCell(newBoard, cellID, (cell) => { cell.isBomb = false });
+        //keeps count of nearby bombs of surrounding cells
+        this.allNearbyCells(newBoard, cellID, (cell) => { cell.nearbyBombs-- })
     }
 
     fixFirstClickBomb(cellID) {
-        console.log(cellID);
+        //what toDo
+        //now i need to remove all of the ids from removeTHeseBombs from this.state.idsOfBombs
+        //so everything is kept track of
 
         let newBoard = this.state.board;
 
-        //this.decrementNearbyCellBomb(newBoard, cellID)
+        let newidsOfBombs = this.state.idsOfBombs;
 
-        //make all nearby cells false
+        let removeTheseBombs = [];
 
-        let theNewNewBoard = this.showNearbyCellBombScreen(newBoard, cellID);
-
+        this.allNearbyCells(newBoard, cellID, (cell) => {
+            if (cell.isBomb) {
+                removeTheseBombs.push(cell.id)
+            }
+        })
+        //remove the cell that was clicked
+        removeTheseBombs.push(cellID)
+        //and any bombs nearby aswell
+        removeTheseBombs.forEach((cell) => {
+            this.removeBomb(newBoard, cell)
+        })
 
         this.setState({
-            board: theNewNewBoard
+            board: newBoard
         })
-        //needs to remove id from idsOfBomb
-        //
-
-        //decrement all sourding ids count
-        //let newArr = this.removeItem(this.state.idsOfBombs, cellID)
-
-        //randomly create new id
-        //add it to newArr
-
-        //make sure it isnt already on idsOfBomb
-
-        /*this.setState({
-            idsOfBombs: newArr
-        })*/
-        //decrement all sourding ids count
-        //
-        //increment all sourdning ids bombs count
     };
-
+    //all this functions really does is mark and unmarks a cell from ? to m
     handleRightClick(e) {
         //stops menu that pops up when right clicked it
         e.preventDefault();
@@ -262,7 +240,6 @@ export default class Board extends Component {
             }
         }
 
-        console.log('fuck')
         this.setState({
             board: newBoard
         })
