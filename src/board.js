a170ea6c3ff1faeef864d5cddde4c505bcd4bdfc
Rafael Herrera <santoElias65537@gmail.com>
2022-01-13 22:11:02 -0700

    deleted a lot of stuff, changed some small stuff inside tableRows function

--- src/board.js
+++ src/board.js
@@ -17,12 +17,12 @@ export default class Board extends Component {
         }
     }
 
+
+
     getBoardReady = () => {
-        this.createNestedArray(8, 8);
+        this.createNestedArray(8, 9);
 
     };
-
-    
 
     createNestedArray = (x, y) => {
         //x is how many arrays
@@ -34,56 +34,61 @@ export default class Board extends Component {
         //[{}, {}], 
         //[{}, {}]
         //]
-        let countBombs = 0;
-        let idsOfBombsHere = [];
 
         let nestedArray = [];
-        for (let i = 0; i < x; i++) {
+
+        for (let i = 0; i < y; i++) {
             nestedArray.push([]);
-            for (let j = 0; j < y; j++) {
+            for (let j = 0; j < x; j++) {
                 //true represents bomb
-                
+
                 //keeps track of trues count && the ids 
-                
+
                 //information of individual cell
                 nestedArray[i][j] = {
-                    screen: "?",
-                  
+                    screen: `${j}-${i}`,
+
                     hasBeenClicked: false,
                     nearbyBombs: 0,
-                    id: `${i}-${j}`,
+                    id: `${j}-${i}`,
                     wasrightClicked: false
                 };
             }
         }
 
-        for (let i = 0; i < idsOfBombsHere.length; i++) {
-            //increments all nearby cells if its a bomb
-            this.allNearbyCells(nestedArray, idsOfBombsHere[i], (bomb) => { bomb.nearbyBombs++ })
-        }
+        nestedArray.reverse()
+
 
         this.setState({
             //keeps track of board and some bomb info
             board: nestedArray,
-            bombsOnBoard: countBombs,
-            idsOfBombs: idsOfBombsHere,
-            firstClick: true,
-            bombsDiscovered: 0,
-            bombsDiscoveredVerified: 0
+            firstClick: true
         })
     }
 
-    //for every item in array creates cell
-    //[[],creates row from array
-    //[{randomObj}], creates cell from randomObj
-    //[{}, {}, {}] row with 3 cells
+    cellClick = (e) => {
+
+        console.log(e.target.id)
+    }
+
+
+    handleRightClick(e) {
+        //stops menu that pops up when right clicked 
+        e.preventDefault();
+        console.log("right click")
+    }
+
+
     tablerows = (nestedArray) => {
-        if (!this.state.board) { return; }//if a board already exist return
+        if (!this.state.board) { return; }//if a board already exists in state return
+
         return nestedArray.map((rows, x) => {
-            let row = rows.map((cell, y) => <td id={`${x}-${y}`} onClick={(e) => { this.cellClick(e); }} onContextMenu={(e) => this.handleRightClick(e)}>
-                {/*if is hasnt been clicked display a question mark, else deplending on the situation display some kind of logic */}
-                {this.state.board[x][y].screen}
-            </td>);
+
+            let row = rows.map((cell, y) =>
+                <td id={nestedArray[x][y].id} onClick={(e) => { this.cellClick(e); }} onContextMenu={(e) => this.handleRightClick(e)}>
+                    {nestedArray[x][y].screen}
+                </td>);
+
             return (
                 <tr>
                     {row}
@@ -92,27 +97,6 @@ export default class Board extends Component {
         });
     }
 
-    //////////////////////////////////////////////////////////////////////////
-    //      Some helper functions
-    /////////////////////////////////////////////////////////////////////////
-
-    allNearbyCells(board, stringId, fn) {
-        let Id = this.stringToId(stringId);
-
-        const x = Id[0];
-        const y = Id[1];
-
-        for (let i = x - 1; i <= x + 1; i++) {
-            for (let j = y - 1; j <= y + 1; j++) {
-                //if it dont exist, move on
-                if (!board[i] || !board[i][j]) { continue; }
-                //dont put the box you clicked on the array of ids that represent the surrounding ids
-                if (i === x && j === y) { continue; }
-
-                fn(board[i][j]);
-            }
-        }
-    }
 
     changeCellScreen(currentBoard, id) {
 
@@ -148,124 +132,10 @@ export default class Board extends Component {
     }
 
 
-    
-
-
-    cellClick = (e) => {
-
-        let newBoard = this.state.board;
-
-     
-        //makes cell unclickable
-        this.getCell(newBoard, e.target.id, (cell) => { cell.hasBeenClicked = true; })
-
-        this.changeCellScreen(newBoard, e.target.id);
-
-        this.setState({
-            board: newBoard,
-            firstClick: false
-        });
-    }
-
-    //all this functions really does is mark and unmarks a cell from ? to m, and keeps count if you marked a bombs successfully
-    handleRightClick(e) {
-        //stops menu that pops up when right clicked 
-        e.preventDefault();
-
-        console.log("right click")
-
-        let newBoard = this.state.board;
-        let cellClicked = this.getCell(newBoard, e.target.id)
-
-        if (cellClicked.hasBeenClicked) { return; }
-        //so you can mark and unmark cell
-        cellClicked.wasrightClicked = !cellClicked.wasrightClicked;
-
-        if (cellClicked.wasrightClicked) {
-            cellClicked.screen = "M";
-            //keeps track of bombs marked
-            this.setState(prevState => { return { bombsDiscovered: prevState.bombsDiscovered + 1 } });
-            //if you marked a cell that really was a bomb, increment counter
-            if (cellClicked.isBomb) {
-                this.setState(prevState => { return { bombsDiscoveredVerified: prevState.bombsDiscoveredVerified + 1 } });
-            }
-        } else {
-            cellClicked.screen = '?';
-            //keeps track of bombs marked
-            this.setState(prevState => {
-                return {
-                    bombsDiscovered: prevState.bombsDiscovered - 1
-                }
-            })
-            if (cellClicked.isBomb) {
-                this.setState(prevState => { return { bombsDiscoveredVerified: prevState.bombsDiscoveredVerified - 1 } })
-            }
-        }
-
-        this.setState({
-            board: newBoard
-        }, () => {
-            this.checkWin();
-        })
-    }
-
-    ///////////////////////////////////////////////
-    //////  main
-    ///////////////////////////////////////////////
-
-    removeBomb(newBoard, cellID) {
-        //makes the cell bomb false
-        this.getCell(newBoard, cellID, (cell) => { cell.isBomb = false });
-        //keeps count of nearby bombs of surrounding cells
-        this.allNearbyCells(newBoard, cellID, (cell) => { cell.nearbyBombs-- })
-    }
-
-    fixFirstClickBomb(cellID) {
-        let newBoard = this.state.board;
-
-        let newidsOfBombs = this.state.idsOfBombs;
-
-        let bombsRemoved = [];
-        //cell clicked
-        this.getCell(newBoard, cellID, (cell) => {
-            if (cell.isBomb) {
-                this.removeBomb(newBoard, cell.id);
-
-                bombsRemoved.push(cellID)
-            }
-        })
-
-        this.allNearbyCells(newBoard, cellID, (cell) => {
-            if (cell.isBomb) {
-                this.removeBomb(newBoard, cell.id);
-
-                bombsRemoved.push(cell.id)
-            }
-        })
-
-        //substract bombsRemoved array from newIdsOfBombs
-        //ex. arr1 = [1, 2, 3, 4], arr2 = [1, 3] //makes it so arr1 = [2, 4]
-        newidsOfBombs = newidsOfBombs.filter((item) =>
-            !bombsRemoved.includes(item)
-        )
-
-        this.setState({
-            board: newBoard,
-            idsOfBombs: newidsOfBombs,
-            bombsOnBoard: newidsOfBombs.length
-        })
-    };
-
-    checkWin() {
-        if (this.state.bombsOnBoard === this.state.bombsDiscoveredVerified) {
-            alert('youve kinda won')
-        }
-    }
-
     render() {
         return (
             <div>
-               
+
                 <button onClick={() => { this.getBoardReady(); }}>Start New Game</button>
                 <table><tbody>{this.tablerows(this.state.board)}</tbody></table>
             </div>
